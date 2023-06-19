390f052c6323b328f7fb0c95a1b2bd83846599f7
mohanraj <mohanrajcgm@gmail.com>
2023-06-19 08:28:06 +0530

    first upload

---
+++ routes/quotes.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { getQuotes, addQuote, updateQuote, deleteQuote, getQuotesById } = require('../Controllers/quotes.controller');
+
+const router = express.Router();
+
+router.get('/quotes', getQuotes);
+
+router.get('/quotes/:quoteId', getQuotesById);
+
+router.post('/quotes', addQuote);
+
+router.put('/quotes/:quoteId', updateQuote);
+
+router.delete('/quotes/:quoteId', deleteQuote);
+
+module.exports = router;
